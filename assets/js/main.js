ef3c8da4c2ffb68df6c0e2d587e8b69e6aac4607
tuilahienne <hientranvan27@gmail.com>
2021-10-25 21:13:30 +0700

    web 30shine again

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+// get banner
+
+var bannerElement = document.getElementById('banner');
+
+var myArrayBanner = [
+    'url("./assets/imgs/211001_BANNER_WEB_30SHINE_w.jpg")',
+    'url("./assets/imgs/210814_Banner_an_toan_w.jpg")',
+    'url("./assets/imgs/210814_Banner_SC_10_BC_w.jpg")'
+]
+var i = 0;
+function showSlide() {
+    bannerElement.style.backgroundImage = myArrayBanner[i];
+    i++;
+    if(i >= myArrayBanner.length) i = 0;
+}
+setInterval(showSlide, 3000)
+
+function clickNextSlide() {
+    showSlide();
+}
+
+// show model login
+
+var loginElement = document.getElementById('btn-login');
+var modelElement = document.getElementById('model')
+loginElement.addEventListener('click', function() {
+    modelElement.classList.add('block-display');
+});
+
+// close model 
+var closeModelElement = document.getElementById('model-close');
+closeModelElement.addEventListener('click', function() {
+    modelElement.classList.remove('block-display');
+})
+
---
+++ assets/js/toast-message.js
@@ -0,0 +1,87 @@
+function toast({title= '', message = '', type = 'Info', duration = 3000}) {
+    const main = document.getElementById('toast');
+    if (main) {
+        const toast = document.createElement('div');
+        //auto remove toast
+        const autoRemoveId = setTimeout(function() {
+            main.removeChild(toast);
+        }, duration + 1000); 
+        //remove toast click
+        toast.onclick = function(e){
+            if(e.target.closest('.toast__close')) {
+                main.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            }
+        }
+        const icons = {
+            success: 'fa fa-check',
+            warning: 'fa fa-engine-warning',
+            error: 'fa fa-exclamation-triangle'
+        };
+        const icon = icons[type];
+        const delay = (duration / 1000).toFixed(2);
+        toast.classList.add('toast', `toast--${type}`);
+        toast.style.animation = `slideInLeft ease 0.3s, fadeOut linear 1s ${delay}s forwards`
+
+        toast.innerHTML = `
+             <div class="toast__icon">
+                <i class="${icon}"></i>
+            </div>
+            <div class="toast__body">
+                <h3 class="toast__title">${title}</h3>
+                <p class="toast__msg">${message}</p>
+            </div>
+            <div class="toast__close">
+                <i class="fas fa-times"></i>
+            </div>
+        `;
+        main.appendChild(toast);
+
+        
+    }
+}
+// toast({
+//     title: "Success",
+//     message: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+//     type: "warning",
+//     duration: 3000
+//    })
+function showSuccessToast() {
+    toast({
+    title: "Thành công",
+    message: "Bạn đã đăng kí thành công",
+    type: "success",
+    duration: 1000
+})
+}
+function showErrorToast() {
+         toast({
+    title: "Thất bại",
+    message: "Bạn chưa đăng kí thành công",
+    type: "error",
+    duration: 3000
+})      
+}
+function showNullInputNumberPhone() {
+    toast({
+        title: "Chú ý",
+        message: "Bạn vui lòng nhập số diện thoại",
+        type: "error",
+        duration: 3000
+    })
+}
+
+// var loginElement = document.getElementById('btn-login');
+// var modelElement = document.getElementById('model')
+// loginElement.addEventListener('click', showSuccessToast
+// );
+
+var btnBannerElement = document.getElementById('banner-model__btn');
+var getTxtInputPhone = document.getElementById('txtInputPhone');
+btnBannerElement.addEventListener('click',function() {
+    if(getTxtInputPhone.value === "") 
+        showNullInputNumberPhone();
+    else {
+        showSuccessToast();
+    }
+});
